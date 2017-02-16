9da6a2dc09fff41dbe50d0995980d347a65e72ad
xinmin.li <xinmin.li@grabtaxi.com>
2017-02-16 14:44:41 +0800

    add janitor

--- cache/cache.go
+++ cache/cache.go
@@ -1,9 +1,10 @@
 package cache
 
 import (
+	"fmt"
 	"sync"
 	"time"
-	"fmt"
+	"runtime"
 )
 
 type Cache struct {
@@ -19,26 +20,25 @@ type Item struct {
 type cache struct {
 	defaultExpiration time.Duration
 	items             map[string]Item
+	janitor           *janitor
 	mu                sync.RWMutex
 }
 
 const (
 	DefaultExpiration time.Duration = 0
-	NoExpiration time.Duration = -1
+	NoExpiration      time.Duration = -1
 )
 
-
 // check if item has expired
-func (item *Item) hasExpired() bool{
+func (item *Item) hasExpired() bool {
 	return item.Expiration > 0 && item.Expiration < time.Now().UnixNano()
 }
-
 
 // get expiration timestamp by time.Duration
 func calcExpirationTime(defaultDuration, duration time.Duration) int64 {
 	if duration > 0 {
 		return time.Now().Add(duration).UnixNano()
-	}else if duration == DefaultExpiration {
+	} else if duration == DefaultExpiration {
 		return defaultDuration
 	}
 	// here duration must < 0 indicate that there is no expiration
@@ -54,12 +54,12 @@ func (c *cache) Set(k string, v interface{}, d time.Duration) {
 
 // add if there isn't an existing item or unexpired item by given k
 // otherwise an error return
-func (c *cache) Add(k string, v interface{}, d time.Duration) error{
+func (c *cache) Add(k string, v interface{}, d time.Duration) error {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
 	item, found := c.items[k]
-	if found && item.hasExpired{
+	if found && item.hasExpired {
 		return fmt.Errorf("there is an item existing by given k %s", k)
 	}
 
@@ -84,11 +84,10 @@ func (c *cache) Replace(k string, v interface{}, d time.Duration) error {
 	return nil
 }
 
-
 // set operation without lock
 func (c *cache) set(k string, v interface{}, d time.Duration) {
 	c.items[k] = Item{
-		Object: v,
+		Object:     v,
 		Expiration: calcExpirationTime(c.defaultExpiration, d),
 	}
 }
@@ -109,25 +108,87 @@ func (c *cache) Get(k string) (interface{}, bool) {
 	return item, true
 }
 
+func (c *cache) DeleteExpired() {
+	now := time.Now().UnixNano()
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for k, v := range c.items {
+		if v.Expiration > 0 && v.Expiration < now {
+			delete(c.items, k)
+		}
+	}
+}
+
 // delete all items
-func (c *cache)Clear() {
+func (c *cache) Clear() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	c.items = map[string]Item{}
 }
 
-func (c *cache)ItemCount() int {
+func (c *cache) ItemCount() int {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	return len(c.items)
 }
 
-func NewCache(defaultExpiration time.Duration) *Cache {
-	items := make(map[string]Item)
-	return &cache{
-		items: items,
-		defaultExpiration: defaultExpiration,
+type janitor struct {
+	stopC    chan bool
+	interval time.Duration
+}
+
+func (j *janitor) Run(c *cache) {
+	j.stopC = make(chan bool)
+	ticker := time.NewTicker(j.interval)
+	for {
+		select {
+		case <-ticker.C:
+			c.DeleteExpired()
+		case <-j.stopC:
+			ticker.Stop()
+			return
+		}
 	}
+}
+
+
+func runJanitor(c *cache, ci time.Duration) {
+	j := &janitor{
+		interval: ci,
+	}
+	c.janitor = j
+
+	go j.Run(c)
+}
+
+func stopJanitor(c *Cache) {
+	c.janitor.stopC <- true
+}
+
+func newCacheWithJanitor(de, ci time.Duration, items map[string]Item) *Cache {
+	if de == 0 {
+		de = NoExpiration
+	}
+	c := &cache{
+		defaultExpiration: de,
+		items:             items,
+	}
+
+	C := &Cache{c}
+	// to make sure after C gc'ed , the Janitor will stop
+	if ci > 0 {
+		runJanitor(c, ci)
+		runtime.SetFinalizer(C, stopJanitor)
+	}
+
+	return C
+}
+
+func New(defaultExpiration, cleanupInterval time.Duration) *Cache {
+	items := map[string]Item{}
+	return newCacheWithJanitor(defaultExpiration, cleanupInterval, items)
 }
